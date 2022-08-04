5350995789c39bc4ca9fba03824db3aac079d589
ArtInLines <linesinart@gmail.com>
2022-08-04 10:17:21 +0200

    Made domInputManager a class

--- src/Input/domInputManager.js
+++ src/Input/domInputManager.js
@@ -2,31 +2,30 @@
  * @author Andrei Kashcha (aka anvaka) / https://github.com/anvaka
  */
 
-module.exports = domInputManager;
-
 var dragndrop = require('./dragndrop.js');
 
-function domInputManager(graph, graphics) {
-	var nodeEvents = {};
-	return {
-		/**
-		 * Called by renderer to listen to drag-n-drop events from node. E.g. for SVG
-		 * graphics we may listen to DOM events, whereas for WebGL the graphics
-		 * should provide custom eventing mechanism.
-		 *
-		 * @param node - to be monitored.
-		 * @param handlers - object with set of three callbacks:
-		 *   onStart: function(),
-		 *   onDrag: function(e, offset),
-		 *   onStop: function()
-		 */
-		bindDragNDrop: bindDragNDrop,
-	};
+class DomInputManager {
+	constructor(graph, graphics) {
+		this.nodeEvents = {};
+		this.graph = graph;
+		this.graphics = graphics;
+	}
 
-	function bindDragNDrop(node, handlers) {
+	/**
+	 * Called by renderer to listen to drag-n-drop events from node. E.g. for SVG
+	 * graphics we may listen to DOM events, whereas for WebGL the graphics
+	 * should provide custom eventing mechanism.
+	 *
+	 * @param node - to be monitored.
+	 * @param handlers - object with set of three callbacks:
+	 *   onStart: function(),
+	 *   onDrag: function(e, offset),
+	 *   onStop: function()
+	 */
+	bindDragNDrop(node, handlers) {
 		var events;
 		if (handlers) {
-			var nodeUI = graphics.getNodeUI(node.id);
+			var nodeUI = this.graphics.getNodeUI(node.id);
 			events = dragndrop(nodeUI);
 			if (typeof handlers.onStart === 'function') {
 				events.onStart(handlers.onStart);
@@ -38,10 +37,12 @@ function domInputManager(graph, graphics) {
 				events.onStop(handlers.onStop);
 			}
 
-			nodeEvents[node.id] = events;
-		} else if ((events = nodeEvents[node.id])) {
+			this.nodeEvents[node.id] = events;
+		} else if ((events = this.nodeEvents[node.id])) {
 			events.release();
-			delete nodeEvents[node.id];
+			delete this.nodeEvents[node.id];
 		}
 	}
 }
+
+module.exports = (graph, graphics) => new DomInputManager(graph, graphics);
