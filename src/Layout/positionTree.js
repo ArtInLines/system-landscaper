e1c1581b31357879551c1a528ff057b0a4384c9a
ArtInLines <linesinart@gmail.com>
2022-08-05 08:30:52 +0200

    Create positionTree.js

---
+++ src/Layout/positionTree.js
@@ -0,0 +1,27 @@
+/**
+ * @typedef {Object} positionTreeOpts
+ * @property {number} levelSeperation The distance between levels of the tree in relative y-coordinates.
+ * @property {number} maxDepth The maximum depth of the tree, that should be positioned.
+ * @property {number} siblingSeperation The distance between sibling-nodes in relative x-coordinates.
+ * @property {number} subtreeSeperation The distance between subtree-nodes in relative x-coordinates. This is usually slightly larger than siblingSeperation.
+ */
+
+/**
+ * Get the position for all nodes in a Tree, such that the Tree is aesthetically arranged. This is an implementation of the Walker's algorithm: https://www.cs.unc.edu/techreports/89-034.pdf. It works on general trees, not just binary trees.
+ * @param {SystemNode} root Root of the (Sub)Tree
+ * @param {positionTreeOpts} opts Options for the positionTree function. These are treated as constants by the function and are not changed.
+ */
+function positionTree(root, opts = { levelSeperation, maxDepth, siblingSeperation, subtreeSeperation }) {
+	// Initialize the list of previous nodes at each level.
+	initPrevNodeList();
+
+	// Do the preliminary positioning with a postorder walk
+	firstWalk(root, 0, opts);
+
+	// Determine how to adjust all the nodes with respect to the location of the root
+	let xTopAdjustment = xcoord(root, opts) - prelim(root, opts);
+	let yTopAdjustment = ycoord(root, opts);
+
+	// Do the final positioning with a preorder walk
+	return seconWalk(root, 0, 0, xTopAdjustment, yTopAdjustment, opts);
+}
